6e71b584b5262d86111c810ebe71c43b607fdb8f
UI906879 <Asdfbpbp11>
2020-01-21 17:04:43 +0000

    reworking after meeting with morning star - mvp

--- mscommod/mp.py
+++ mscommod/mp.py
@@ -2,41 +2,39 @@ import pandas as pd
 import io
 import requests
 from functools import lru_cache
-import logging
-import datetime
-import re
 import os
-import xml.etree.ElementTree as ET
 from requests.auth import HTTPBasicAuth
 
 feed_url = 'https://mp.morningstarcommodity.com/lds/feeds/{}'
 ts_url = 'https://mp.morningstarcommodity.com/lds/feeds/{}/ts?{}={}'
-fwd_url = 'https://mp.morningstarcommodity.com/lds/feeds/{}/ts?Contract={}'
+curve_url = 'https://mp.morningstarcommodity.com/lds/feeds/{}/curve?'
 feed_utl = 'https://mp.morningstarcommodity.com/lds/users/{}/feeds'
+search_url = "https://mp.morningstarcommodity.com/lds/search/v2/search"
+query_url = "https://mp.morningstarcommodity.com/lds/formulas/run/js"
 
 mpUserName = os.environ['MPUSERNAME'].replace('"', '')
 mpPassword = os.environ['MPPASSWORD'].replace('"', '')
+auth = requests.auth.HTTPBasicAuth(mpUserName, mpPassword)
 
-headers = {
+headers_csv = {
         'Accept': "text/csv",
         'cache-control': "no-cache"
 }
 
-def url_to_dataframe(u, headers=headers):
-    r = requests.get(u, auth=requests.auth.HTTPBasicAuth(mpUserName, mpPassword), headers=headers)
+headers_json = {
+    'Content-Type': 'application/json'
+}
+
+
+def url_to_dataframe(u, headers=headers_csv):
+    r = requests.get(u, auth=auth, headers=headers)
     df = pd.read_csv(io.StringIO(r.text), index_col='Date')
     df.index = pd.to_datetime(df.index)
     return df
 
 
-def ts(symbol, feed, key='Symbol'):
-    u = ts_url.format(feed, key, symbol)
-    df = url_to_dataframe(u)
-    return df
-
-
 @lru_cache(maxsize=None)
-def getFeed(feed):
+def feed(feed):
     u = feed_url.format(feed)
     r = requests.get(u, auth=requests.auth.HTTPBasicAuth(mpUserName, mpPassword))
     d = pd.DataFrame(r.json())
@@ -47,161 +45,89 @@ def getFeed(feed):
 
 
 @lru_cache(maxsize=None)
-def feedKey(feed):
-    df = getFeed(feed)
-    df = df[df['fieldType'] == 'k']
+def feedkey(feedname):
+    # some caching
+    if feedname.startswith('Platts'):
+        return 'Code'
+
+    if feedname.startswith('ICE'):
+        if feedname.endswith('_continuous'):
+            return 'Contract'
+        else:
+            return 'Symbol'
+
+    df = feed(feedname)
+    df = df[df['fieldType'].str.lower() == 'k']
     if len(df) == 1:
         return df['fieldName'].iloc[0]
 
 
-def fwd(symbol, feed, contract='001_Month'):
-    symbol = '{}_{}'.format(symbol, contract)
-    u = fwd_url.format(feed, symbol)
+@lru_cache(maxsize=None)
+def search(symbol):
+    j = dict()
+    criteriaons = []
+    d1 = dict()
+    d1["type"] = "QUERY"
+    d1["value"] = symbol
+    criteriaons.append(d1)
+
+    j["criterions"] = criteriaons
+    j["entitlement"] = "all"
+    j["isFuturesOnly"] = "false"
+    j["isExcludeFutures"] = "false"
+    j["hideExpiredContracts"] = "true"
+    r = requests.post(search_url, json=j, auth=requests.auth.HTTPBasicAuth(mpUserName, mpPassword), headers=headers_json)
+
+    if r.status_code == 200:
+        return r.json()
+
+
+def _find_symbol_metadata(symbol, feedname=None, feedkeyname=None, inc_feeddata=False):
+    if feedname is None:
+        searchres = search(symbol)
+        if isinstance(searchres, dict) and len(searchres['results']) >= 1:
+            if 'feed' in searchres['results'][0]:
+                feedname = searchres['results'][0]['feed']
+
+    if feedkeyname is None:
+        feedkeyname = feedkey(feedname)
+
+    if inc_feeddata:
+        feeddata = feed(feedname)
+        return feedname, feedkeyname, feeddata
+
+    return feedname, feedkeyname
+
+
+def series(symbol, feedname=None, feedkeyname=None):
+    feedname, feedkeyname = _find_symbol_metadata(symbol, feedname, feedkeyname)
+    u = ts_url.format(feedname, feedkeyname, symbol)
     df = url_to_dataframe(u)
     return df
 
 
-def mpLogger(msg):
-    logging.info(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S.%Z") + ' ' + msg)
+def query(querystr):
+    r = requests.request("POST", query_url, headers=headers_csv, data=querystr, auth=auth)
+    df = pd.read_csv(io.StringIO(r.text), header=None)
+    df = df.set_index(0)
+    df.index = pd.to_datetime(df.index)
+    return df
 
 
-
-def getSources(pattern='.*'):
-    """
-    Returns an array of available Marketplace data sources entitled for the user
-
-    :Example:
-    >>> getSources()
-    ['Gfi', 'WallStreetHorizon', 'Eoxlive', ...
-
-    :Example:
-    >>> getSources('Wall.*')
-    ['WallStreetHorizon']
-
-    """
-    global mpUserName
-    global mpPassword
-
-    # URL to request the sources
-    mpurl = "https://mp.morningstarcommodity.com/lds/users/" + mpUserName + "/feeds"
-    # change the username and password here - must be in quotes
-    r = requests.get(mpurl, auth=HTTPBasicAuth(mpUserName, mpPassword))
-    mpSources = []
-    for x in r.json():
-        if x["dataSource"] not in mpSources:
-            mpSources.append(x["dataSource"])
-
-    mpSources.sort()
-    r = re.compile(pattern)
-    return list(filter(r.match, mpSources))
-
-
-@lru_cache(maxsize=None)
-def getFeeds(source, pattern='.*'):
-    mpurl = "https://mp.morningstarcommodity.com/lds/users/" + mpUserName + "/feeds"
-    # change the username and password here - must be in quotes
-    r = requests.get(mpurl, auth=HTTPBasicAuth(mpUserName, mpPassword))
-    df = pd.DataFrame(r.json())['name']
-    val = list(df.sort_values())
-    val = [x for x in val if source in x]
-    regex = re.compile(pattern)
-    res = list(filter(regex.match, val))
+def curve(symbol, feedname=None, feedkeyname=None, column='Settlement_Price', curvedate='today()'):
+    feedname, feedkeyname = _find_symbol_metadata(symbol, feedname, feedkeyname)
+    q = """
+        var $prod = morn.Product.create("{}", ["{}"], ["{}"]);
+        var Source = forward_curve($prod, get_curve_date($prod, {}), "Month");
+        Source = drop_nans(Source);
+        """
+    q = q.format(feedname, symbol, column, curvedate)
+    res = query(q)
+    res = res.rename(columns={res.columns[0]:symbol})
     return res
-
-
-@lru_cache(maxsize=None)
-def getCurveNames(feed):
-    """
-    Returns an array of curve names available for a feed
-
-    :param feed: name of feed
-
-    :Example:
-    >>> getCurveNames('ICE_EuroFutures')
-    ['AF1', 'AF2', 'AF3'...
-
-    """
-    global mpUserName
-    global mpPassword
-    availableCurves = "https://mp.morningstarcommodity.com/lds/feeds/" + feed + "/contractroots"
-    r = requests.get(availableCurves, auth=requests.auth.HTTPBasicAuth(mpUserName, mpPassword))
-    if r.headers['content-type'] == 'application/json':
-        mpCurves = r.json()["root"]
-        mpCurves.sort()
-        return mpCurves
-    else:
-        msg = []
-        root = ET.fromstring(r.content)
-        for errors in root:
-            msg.append(errors.tag + ": " + errors.text)
-        return msg
-
-
-@lru_cache(maxsize=None)
-def getKeyNames(feed):
-    """
-    Returns an array of keys used in a feed
-
-    :param feed: name of feed
-
-    :Example:
-    >>> getKeyNames('ICE_EuroFutures')
-    ['Symbol']
-
-    """
-    global mpUserName
-    global mpPassword
-    availableCurves = "https://mp.morningstarcommodity.com/lds/feeds/" + feed + "/keys?limit=1000"
-    r = requests.get(availableCurves, auth=HTTPBasicAuth(mpUserName, mpPassword))
-    allKeys = []
-    if r.headers['content-type'] == 'application/json':
-        df = pd.DataFrame(r.json())
-        df['key'] = df.apply(lambda x: x['keys'][0]['value'], 1)
-        df = df[df['desc'] != 'totalRecords']
-        keys = list(df['key'].values)
-        keys.sort()
-        allKeys.append(keys)
-        return allKeys[0]
-    else:
-        msg = []
-        root = ET.fromstring(r.content)
-        for errors in root:
-            msg.append(errors.tag + ": " + errors.text)
-        return msg
-
-
-def getColumns(feed):
-    """
-    Returns an array of columns used in a feed
-
-    :param feed: name of feed
-
-    :Example:
-    >>> getColumns('ICE_EuroFutures')
-
-
-    ['Settlement_Price']
-
-    """
-    global mpUserName
-    global mpPassword
-    availableCurves = "https://mp.morningstarcommodity.com/lds/feeds/" + feed
-    r = requests.get(availableCurves, auth=HTTPBasicAuth(mpUserName, mpPassword))
-    cols = []
-    if r.headers['content-type'] == 'application/json':
-        for x in range(len(r.json()['fields'])):
-            if r.json()['fields'][x]['type'].upper() == 'V':
-                cols.append(r.json()['fields'][x]['fieldName'])
-        return cols
-    else:
-        msg = []
-        root = ET.fromstring(r.content)
-        for errors in root:
-            msg.append(errors.tag + ": " + errors.text)
-        return msg
-
 
 
 if __name__ == '__main__':
     # fwd('BRN', feed='ICE_EuroFutures_continuous')
-    getKeyNames('ICE_EuroFutures')
+    # getKeyNames('ICE_EuroFutures')
+    print('test')
--- test/test_mp.py
+++ test/test_mp.py
@@ -5,30 +5,58 @@ import unittest
 
 class TestMP(unittest.TestCase):
 
-    def test_getSources(self):
-        res = mp.getSources()
-        self.assertIn('ICE', res)
+    def test_feed(self):
+        res = mp.feed('Platts_EB')
+        self.assertEqual(res['name'].drop_duplicates().iloc[0], 'Platts_EB')
+        self.assertEqual(res['dataSource'].drop_duplicates().iloc[0], 'Platts')
 
-    def test_getFeeds(self):
-        res = mp.getFeeds('ICE')
-        self.assertIn('ICE_Canada_FuturesPrices', res)
+    def test_feedkey(self):
+        res = mp.feedkey('Platts_EB')
+        self.assertEqual(res, 'Code')
 
-    def getKeyNames(self):
-        res = mp.getKeyNames('ICE_EuroFutures')
-        self.assertIn('AF30Q', res)
+        res = mp.feedkey('ICE_EuroFutures')
+        self.assertEqual(res, 'Symbol')
+
+    def test_search(self):
+        res = mp.search('AAIBI00')
+        self.assertEqual(len(res['results']), 1)
+        self.assertEqual(res['results'][0]['feed'], 'Platts_AE')
+
+    def test_query(self):
+        q = """
+        var $Source = morn.Product.create("CME_NymexFutures_EOD", ["NG"], ["Settlement_Price"] );
+        var Source = forward_curve($Source, get_curve_date($Source, today()), "Month");
+        Source = drop_nans(Source);
+        """
+        res = mp.query(q)
+        self.assertEqual(type(res.index), pd.core.indexes.datetimes.DatetimeIndex)
 
     def test_getts1(self):
-        res = mp.ts('BRN-Q0F', 'ICE_EuroFutures')
+        res = mp.series('BRN-Q0F', 'ICE_EuroFutures')
         self.assertEqual(res.loc[pd.to_datetime('2017-03-31'),'settlement_price(BRN-Q0F)'], 53.32)
 
+    def test_getts1a(self):
+        res = mp.series('BRN_001_Month',)# 'ICE_EuroFutures_continuous')
+        self.assertEqual(res.loc[pd.to_datetime('2020-01-02'),'settlement_price(BRN_001_Month)'], 66.25)
+
     def test_getts2(self):
-        res = mp.ts('PJABA00', 'Platts_EB', key='Code')
+        res = mp.series('PJABA00', 'Platts_EB', feedkeyname='Code')
         self.assertEqual(res.loc[pd.to_datetime('2019-11-04'),'close(PJABA00)'], 635.25)
 
+    def test_getts3(self):
+        res = mp.series('AAIBI00')
+        self.assertEqual(res.loc[pd.to_datetime('2019-12-01'), 'close(AAIBI00)'], 713.65)
 
-    def test_getfwd(self):
-        res = mp.fwd('BRN', 'ICE_EuroFutures_continuous')
-        self.assertEqual(res.loc[pd.to_datetime('2009-11-16'),'settlement_price(BRN_001_Month)'], 78.76)
+    def test_getCurve(self):
+        res = mp.curve('BRN', 'ICE_EuroFutures')
+        self.assertEqual(res.columns[0], 'BRN')
+
+        res = mp.curve('BRN')
+        self.assertEqual(res.columns[0], 'BRN')
+
+        res = mp.curve('BRN', 'ICE_EuroFutures', curvedate='2020-01-02')
+        self.assertEqual(res.columns[0], 'BRN')
+        self.assertEqual(res['BRN']['2020-03-01'], 65.20)
 
 
 if __name__ == '__main__':
